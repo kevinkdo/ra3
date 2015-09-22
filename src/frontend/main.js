5a0ffb0a70a32f2edca5bc9b2973ce1b1a1a2756
Kevin Do <kkd10@duke.edu>
2015-09-22 06:32:40 +0000

    Enable typing

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,40 +1,57 @@
+function getDefaultCommandData() {
+  return [
+    {
+      query: "\\select_{bar = 'James Joyce Pub'} Frequents;",
+      result: "Output schema: (drinker varchar, bar varchar, times_a_week int2)\n"
+      + "-----\n"
+      + "Eve|James Joyce Pub|2\n"
+      + "Dan|James Joyce Pub|1\n"
+      + "Amy|James Joyce Pub|2\n"
+      + "Ben|James Joyce Pub|1\n"
+      + "-----\n"
+      + "Total number of rows: 4"
+    },
+    {
+      query: "\\select_{bar = 'Some other pub'} Frequents)",
+      result: "drinker1\ndrinker2\ndrinker3"
+    }
+  ];
+}
+
 var ExampleApplication = React.createClass({
   getInitialState: function() {
     return {
-      commands: [
-        {
-          query: "\\select_{bar = 'James Joyce Pub'} Frequents;",
-          result: "Output schema: (drinker varchar, bar varchar, times_a_week int2)\n"
-          + "-----\n"
-          + "Eve|James Joyce Pub|2\n"
-          + "Dan|James Joyce Pub|1\n"
-          + "Amy|James Joyce Pub|2\n"
-          + "Ben|James Joyce Pub|1\n"
-          + "-----\n"
-          + "Total number of rows: 4\n"
-        },
-        {
-          query: "\\select_{bar = 'Some other pub'} Frequents)",
-          result: "drinker1\ndrinker2\ndrinker3"
-        }
-      ],
-      currentInput: ""
+      commands: getDefaultCommandData(),
+      currentInput: "this is what I'm typing"
     };
   },
 
-  render: function() {
-    var renderedCommands = [];
-    this.state.commands.forEach(function(x) {
-      renderedCommands.push(<QueryResultPair query={x.query} result={x.result} />);//x.query + "\n" + x.result;
-    });
+  handleKeyPress: function(e) {
+    var newCurrentInput = this.state.currentInput + String.fromCharCode(e.which);
+    console.log(String.fromCharCode(e.charCode));
+    this.setState({currentInput: newCurrentInput});
+  },
 
-    return <pre>{renderedCommands}</pre>;
+  render: function() {
+    var renderedLines = [];
+    this.state.commands.forEach(function(x) {
+      renderedLines.push(<QueryResultPair query={x.query} result={x.result} />);
+    });
+    renderedLines.push(<CurrentInput input={this.state.currentInput} />);
+
+    return <pre tabIndex="0" onKeyDown={this.handleKeyPress}>{renderedLines}</pre>;
+  }
+});
+
+var CurrentInput = React.createClass({
+  render: function() {
+    return <span><span className="raprompt">ra&gt; </span>{this.props.input}</span>;
   }
 });
 
 var QueryResultPair = React.createClass({
   render: function() {
-    return <span><span className="raprompt">ra&gt; </span>{this.props.query}{"\n"}{this.props.result}</span>;
+    return <span><span className="raprompt">ra&gt; </span>{this.props.query}{"\n"}{this.props.result}{"\n"}</span>;
   }
 });
 
