b95a408abd00d99af3ccccd32c523a3010ed342d
Michael Han <michael.mj.han@gmail.com>
2015-10-01 15:31:47 -0400

    autocorrect works, not thoroughly tested

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,6 +1,7 @@
 var BACKSPACE = 8;
 var ENTER = 13;
 var TAB = 9;
+var raCommands = ["project_{", "join", "select_{", "cross", "union", "diff", "intersect", "rename_{"];
 
 function getDefaultCommandData() {
   return [
@@ -34,33 +35,63 @@ var ExampleApplication = React.createClass({
       color: "#0f0"
     };
   },
+
+  autocorrect: function(partialCommand)
+  { 
+    if (partialCommand.charAt(0) != '\\') {
+      return -1;
+    }
+    partialCommand = partialCommand.substring(1);
+    var raCommandCharacterMap = {};
+    for (var i = 0; i < raCommands.length; i++) {
+      raCommandCharacterMap[raCommands[i]] = {};
+      for (var j = 0; j < raCommands[i].length; j++) {
+        if (!(raCommands[i][j] in raCommandCharacterMap[raCommands[i]])) {
+          raCommandCharacterMap[raCommands[i]][raCommands[i][j]] = 0;
+        }
+        raCommandCharacterMap[raCommands[i]][raCommands[i][j]]++;
+      }
+    }
+    var distFromCorrect = 10000;
+    var currClosestCommand = "";
+    var minDiff = -1000;
+    for (var i = 0; i < raCommands.length; i++) {
+      var currDiff = 0;
+      var currDistFromCorrect = 0;
+      for (var j = 0; j < partialCommand.length; j++) {
+        if (partialCommand[j] in raCommandCharacterMap[raCommands[i]] && raCommandCharacterMap[raCommands[i]][partialCommand[j]] > 0) {
+          raCommandCharacterMap[raCommands[i]][partialCommand[j]]--;
+        } else {
+          currDiff--;
+        }
+        currDistFromCorrect += Math.abs(j - raCommands[i].indexOf(partialCommand[j]));
+      }
+      console.log(currDiff);
+      console.log(raCommands[i]);
+      console.log("dist: " + currDistFromCorrect);
+
+      if (currDiff >= minDiff && currDistFromCorrect < distFromCorrect) {
+        minDiff = currDiff;
+        distFromCorrect = currDistFromCorrect;
+        currClosestCommand = raCommands[i];
+      }
+    }
+    return currClosestCommand;
+
+  },
+
   //http://stackoverflow.com/questions/1573053/javascript-function-to-convert-color-names-to-hex-codes
   colourNameToHex: function(colour)
   {
-    var colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
-    "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
-    "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
-    "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
-    "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
-    "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
-    "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
-    "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
-    "honeydew":"#f0fff0","hotpink":"#ff69b4",
-    "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
-    "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
-    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
-    "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
-    "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
-    "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
-    "navajowhite":"#ffdead","navy":"#000080",
-    "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
-    "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
-    "red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
-    "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
-    "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
-    "violet":"#ee82ee",
-    "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
-    "yellow":"#ffff00","yellowgreen":"#9acd32", "ghost protocol": "#000000"};
+    var colours = {"aqua":"#00ffff", "azure":"#f0ffff", "black":"#000000", "blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a",
+    "coral":"#ff7f50","crimson":"#dc143c","cyan":"#00ffff", "darkblue":"#00008b","darkcyan":"#008b8b","darkgray":"#a9a9a9","darkgreen":"#006400",
+    "fuchsia":"#ff00ff", "ghostwhite":"#f8f8ff","gold":"#ffd700","gray":"#808080","green":"#008000","greenyellow":"#adff2f", "hotpink":"#ff69b4",
+    "indigo":"#4b0082", "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff",
+    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa",
+    "lime":"#00ff00","limegreen":"#32cd32", "magenta":"#ff00ff","maroon":"#800000", "navy":"#000080", "orange":"#ffa500","orangered":"#ff4500",
+    "peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd", "purple":"#800080", "red":"#ff0000","royalblue":"#4169e1","salmon":"#fa8072",
+    "seagreen":"#2e8b57","seashell":"#fff5ee","silver":"#c0c0c0","skyblue":"#87ceeb","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
+    "tan":"#d2b48c","teal":"#008080","tomato":"#ff6347","turquoise":"#40e0d0", "violet":"#ee82ee", "white":"#ffffff", "yellow":"#ffff00","ghost protocol": "#000000"};
 
     if (typeof colours[colour.toLowerCase()] != 'undefined')
         return colours[colour.toLowerCase()];
@@ -112,13 +143,18 @@ var ExampleApplication = React.createClass({
       }
     } else if (e.keyCode == TAB) {
         e.preventDefault();
-        var tabIndex = this.findPlaceOfTab(this.state.currentInput);
+        /*var tabIndex = this.findPlaceOfTab(this.state.currentInput);
         var toBeCompleted = this.state.currentInput.substring(tabIndex + 1);
+        console.log(toBeCompleted);
         var raCommand = this.autoComplete(toBeCompleted)
         if (raCommand == false) {
           raCommand = toBeCompleted;
         }
-        console.log(raCommand);
+        this.setState({currentInput: this.state.currentInput.substring(0, tabIndex) + "\\" + raCommand});*/
+        var tabIndex = this.findPlaceOfTab(this.state.currentInput);
+        var toBeCompleted = this.state.currentInput.substring(tabIndex);
+        console.log(toBeCompleted);
+        var raCommand = this.autocorrect(toBeCompleted);
         this.setState({currentInput: this.state.currentInput.substring(0, tabIndex) + "\\" + raCommand});
     }
 
