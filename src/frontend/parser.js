1976283b78c59e30f73fe01099f10d50d0114ba9
Kevin Do <kevin.kydat.do@gmail.com>
2015-12-16 15:09:09 -0500

    RunASTNode with equality select condition works

--- src/frontend/parser.js
+++ src/frontend/parser.js
@@ -4,56 +4,79 @@ var beers;
 var request2 = new XMLHttpRequest();
 request2.open('GET', 'beers.json');
 request2.onreadystatechange = function() {
-    if (request2.readyState == 4 && request2.status == 200) {
-        beers = JSON.parse(request2.responseText);
+  if (request2.readyState == 4 && request2.status == 200) {
+    beers = JSON.parse(request2.responseText);
 
-        beers.table_names.forEach(function(table) {
-            tab_options.push(table);
-            beers.tables[table].columns.forEach(function(column) {
-                tab_options.push(column);
-            })
-        });
-    }
+    beers.table_names.forEach(function(table) {
+      tab_options.push(table);
+      beers.tables[table].columns.forEach(function(column) {
+        tab_options.push(column);
+      })
+    });
+  }
 };
 request2.send();
 
 var runQuery = function(query) {
-    var ast = ra_parser.parse(query);
-    return runAstNode(ast);
+  var ast = ra_parser.parse(query);
+  return runAstNode(ast);
 };
 
 var runAstNode = function(node) {
-    var isError = false;
-    var tuples = [];
-    var columns = [];
-    var error_message = "";
-    if (node.name == "\u03c3") { // select
-    } else if (node.name == "\u03C0") { // project
-        var table = beers[node.name];
-    } else if (node.name == "\u00d7") { // cross
-    } else if (node.name == "\u22c8") { // join
-    } else if (node.name == "\u222a") { // union
-    } else if (node.name == "\u2212") { // diff
-    } else if (node.name == "\u2229") { // intersection
-    } else if (node.name == "\u03c1") { // rename
-    } else { // table name
-        if (beers.table_names.indexOf(node.name) > -1) {
-            tuples = beers.tables[node.name].tuples;
-            columns = beers.tables[node.name].columns;
-        } else {
-            isError = true;
-            error_message = "Table does not exist";
-        }
+  var isError = false;
+  var tuples = [];
+  var columns = [];
+  var error_message = "";
+  if (node.name == "\u03c3") { // select
+    try {
+      debugger;
+      var child_result = runAstNode(node.children[0]);
+      var parsed_select_cond = select_cond_parser.parse(node.subscript);
+      if (child_result.isError) {
+        isError = true;
+        error_message = child_result .error_message;
+      }
+      else if (child_result.columns.indexOf(parsed_select_cond.column) == -1) {
+        isError = true;
+        error_message = "Column does not exist: " + parsed_select_cond.column;
+      }
+      else {
+        columns = child_result.columns;
+        tuples = child_result.tuples.filter(function(tuple) {
+          if (parsed_select_cond.oper == "=") {
+            return tuple[parsed_select_cond.column] == parsed_select_cond.val;
+          } // DO REST OF THE CONDITIONS
+        });
+      }
+    } catch (e) {
+      isError = true;
+      error_message = e.message;
     }
+  } else if (node.name == "\u03C0") { // project
+  } else if (node.name == "\u00d7") { // cross
+  } else if (node.name == "\u22c8") { // join
+  } else if (node.name == "\u222a") { // union
+  } else if (node.name == "\u2212") { // diff
+  } else if (node.name == "\u2229") { // intersection
+  } else if (node.name == "\u03c1") { // rename
+  } else { // table name
+    if (beers.table_names.indexOf(node.name) > -1) {
+      tuples = beers.tables[node.name].tuples;
+      columns = beers.tables[node.name].columns;
+    } else {
+      isError = true;
+      error_message = "Table does not exist";
+    }
+  }
 
-    return {
-        isError: isError,
-        columns: columns,
-        tuples: tuples,
-        error: {
-            message: error_message,
-            start: 0,
-            end: 0,
-        }
-    };
+  return {
+    isError: isError,
+    columns: columns,
+    tuples: tuples,
+    error: {
+      message: error_message,
+      start: 0,
+      end: 0,
+    }
+  };
 };
---
+++ src/frontend/select_cond.pegjs
@@ -0,0 +1,56 @@
+{
+  function makeInt(o) {
+    return parseInt(o.join(""), 10);
+  }
+
+  function unit(column, oper, val) {
+    return {
+      column: column,
+      oper: oper,
+      val: val
+    };
+  }
+}
+
+start
+  = WS* n:exp WS* { return n; }
+
+exp
+  = NOT WS* exp_unit
+  / exp_unit WS* AND WS* exp_unit
+  / exp_unit WS* OR WS* exp_unit
+  / n:exp_unit { return n; }
+
+exp_unit
+  = s:COLUMN WS* EQUAL WS* v:val { return unit(s, "=", v); }
+  / s:COLUMN WS* LT WS* v:val { return unit(s, "<", v); }
+  / s:COLUMN WS* LE WS* v:val { return unit(s, "<=", v); }
+  / s:COLUMN WS* GT WS* v:val { return unit(s, ">", v); }
+  / s:COLUMN WS* GE WS* v:val { return unit(s, ">=", v); }
+  / s:COLUMN WS* NE WS* v:val { return unit(s, "<>", v); }
+  / LEFT_PAREN WS* n:exp WS* RIGHT_PAREN { return n; }
+
+val
+  = i:NUMBER { return i; }
+  / QUOTE s:ALPHANUM QUOTE { return s; }
+
+DIGIT = s:[0-9] { return s; }
+NUMBER = s:[0-9]+ { return makeInt(s); }
+ALPHA = s:([a-zA-Z]+) { return s.join(""); }
+ALPHANUM = s:([a-zA-Z0-9]+) { return s.join(""); }
+COLUMN = s1:ALPHA s2:(ALPHA/DIGIT/'_')* { return s1 + s2.join(""); }
+INSIDE_QUOTE = s:([^'])+ { return s.join(""); }
+
+WS = [ \t\r\n]
+LEFT_PAREN = '('
+RIGHT_PAREN = ')'
+QUOTE = '\''
+AND = 'and'
+OR = 'or'
+NOT = 'not'
+EQUAL = '='
+LT = '<'
+LE = '<='
+GT = '>'
+GE = '>='
+NE = '<>'
