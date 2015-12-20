8321c2de0655c2faa4dd2f6ecde201bdb300d32f
Kevin Do <kevin.kydat.do@gmail.com>
2015-12-20 01:07:21 -0500

    Finish \union code generation

--- src/frontend/parser.js
+++ src/frontend/parser.js
@@ -149,6 +149,53 @@ var runAstNode = function(node) {
     }
   } else if (node.name == "\u22c8") { // join
   } else if (node.name == "\u222a") { // union
+    var child_result0 = runAstNode(node.children[0]);
+    var child_result1 = runAstNode(node.children[1]);
+    if (child_result0.isError || child_result1.isError) {
+      result_isError = true;
+      result_error_message = child_result0.isError ?
+        child_result0.error_message : child_result1.error_message;
+    }
+
+    if (!result_isError) {
+      if (child_result0.columns.length !== child_result1.columns.length) {
+        result_isError = true;
+        result_error_message = "Can't union tuples with different number of columns";
+      }
+    }
+
+    if (child_result0.tuples.length === 0) {
+      result_columns = child_result1.columns;
+      result_tuples = child_result1.tuples;
+    }
+
+    if (child_result1.tuples.length === 0) {
+      result_columns = child_result0.columns;
+      result_tuples = child_result0.tuples;
+    }
+
+    if (result_columns.length === 0 && !result_isError) {
+      for (var i = 0; i < child_result0.columns.length; i++) {
+        var type0 = typeof child_result0.tuples[0][child_result0.columns[i]];
+        var type1 = typeof child_result1.tuples[0][child_result1.columns[i]];
+        if (type0 !== type1) {
+          result_isError = true;
+          result_error_message = "Children of \\union have different types: " + type0 + ", " + type1;
+        }
+      }
+    }
+
+    if (result_columns.length === 0 && !result_isError) {
+      result_columns = child_result0.columns;
+      var renamed_child1_tuples = child_result1.tuples.map(function(tuple) {
+        var new_tuple = {};
+        for (var i = 0; i < result_columns.length; i++) {
+          new_tuple[result_columns[i]] = tuple[child_result1.columns[i]];
+        }
+        return new_tuple;
+      });
+      result_tuples = deduplicate_tuples(result_columns, child_result0.tuples.concat(renamed_child1_tuples));
+    }
   } else if (node.name == "\u2212") { // diff
   } else if (node.name == "\u2229") { // intersection
   } else if (node.name == "\u03c1") { // rename
